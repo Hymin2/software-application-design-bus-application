9d2b74a44a8e704639f89341406b0e6d16de4310
scott13579 <105695860+scott13579@users.noreply.github.com>
2022-11-21 19:04:59 +0900

    메인화면 검색바 완료

--- app/src/main/java/com/example/bus_application/MainActivity.kt
+++ app/src/main/java/com/example/bus_application/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.bus_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.bus_application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-
 }
+
