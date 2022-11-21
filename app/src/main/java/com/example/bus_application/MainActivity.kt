c806d68682ae004bef4a1f3ca421d1cd876b1a23
scott13579 <105695860+scott13579@users.noreply.github.com>
2022-11-21 20:19:40 +0900

    메인화면 리사이클러뷰 작업 중

--- app/src/main/java/com/example/bus_application/MainActivity.kt
+++ app/src/main/java/com/example/bus_application/MainActivity.kt
@@ -2,21 +2,42 @@ package com.example.bus_application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.KeyEvent
-import android.view.inputmethod.EditorInfo
-import android.widget.TextView
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.bus_application.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    lateinit var profileAdapter: ProfileAdapter
+    val datas = mutableListOf<ProfileData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        initRecycler()
+    }
+    private fun initRecycler() {
+        profileAdapter = ProfileAdapter(this)
+        binding.rvProfile.adapter = profileAdapter
+
+
+        datas.apply {
+            add(ProfileData(name = "mary", age = 24))
+            add(ProfileData(name = "jenny", age = 26))
+            add(ProfileData(name = "jhon", age = 27))
+            add(ProfileData(name = "ruby", age = 21))
+            add(ProfileData(name = "yuna", age = 23))
+            add(ProfileData(name = "yu21312na", age = 23))
+
+            profileAdapter.datas = datas
+            profileAdapter.notifyDataSetChanged()
+
+        }
     }
 }
+
+
 
---
+++ app/src/main/java/com/example/bus_application/ProfileData.kt
@@ -0,0 +1,6 @@
+package com.example.bus_application
+
+data class ProfileData (
+    val name : String,
+    val age : Int
+)
