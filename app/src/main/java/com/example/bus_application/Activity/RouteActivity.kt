4bb5dd631dcf5b279b4243f031531778fcee6944
scott13579 <105695860+scott13579@users.noreply.github.com>
2022-12-04 01:45:04 +0900

    버스노선도 리사이클러뷰

--- app/src/main/java/com/example/bus_application/Activity/RouteActivity.kt
+++ app/src/main/java/com/example/bus_application/Activity/RouteActivity.kt
@@ -2,6 +2,8 @@ package com.example.bus_application.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bus_application.Adapter.BusRouteAdapter
 import com.example.bus_application.databinding.ActivityRouteBinding
 
 class RouteActivity : AppCompatActivity() {
@@ -12,6 +14,13 @@ class RouteActivity : AppCompatActivity() {
 
         binding = ActivityRouteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.run {
+            rvRouteList.apply{
+                adapter = BusRouteAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
 
     }
 }
