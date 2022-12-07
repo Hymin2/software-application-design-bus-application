7d120381fc96a59e0ba0f58c602fd1cb363d586f
Hymin2 <96197581+Hymin2@users.noreply.github.com>
2022-12-08 07:33:48 +0900

    버스 실시간 갱신 안됨

--- app/src/main/java/ac/kr/tukorea/bus_application/View/Activity/MainActivity.kt
+++ app/src/main/java/ac/kr/tukorea/bus_application/View/Activity/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var BookmarkAdapter : BookmarkAdapter
+    private lateinit var db : AppDatabase
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +43,8 @@ class MainActivity : AppCompatActivity() {
                 rvBookmark.visibility = View.VISIBLE
                 textRide.visibility = View.GONE
                 textGetoff.visibility = View.GONE
+                btnRide.visibility = View.GONE
+                btnGetoff.visibility = View.GONE
 
                 Thread(Runnable {
                     var items = db.bookmarkDao().getAllBookmark()
@@ -55,11 +58,79 @@ class MainActivity : AppCompatActivity() {
                 rvBookmark.visibility = View.GONE
                 textRide.visibility = View.VISIBLE
                 textGetoff.visibility = View.GONE
+                btnGetoff.visibility = View.GONE
+                Thread(Runnable {
+                    var alarmRiding = db.alarmRidingDao()
+                    var text = "설정된 알람이 없습니다."
+                    var bool = alarmRiding.isEmptyAlarmRiding()
+                    if(!bool){
+                        text = alarmRiding.getAlarmRiding().route_name + "\n" + alarmRiding.getAlarmRiding().stop_name
+                    }
+
+                    runOnUiThread {
+                        if(bool) {
+                            textRide.text = text
+                            btnRide.visibility = View.GONE
+                        }
+                        else{
+                            textRide.text = text
+                            btnRide.visibility = View.VISIBLE
+                        }
+                    }
+                }).start()
             }
             radioBtnGetoff.setOnClickListener {
                 rvBookmark.visibility = View.GONE
                 textRide.visibility = View.GONE
                 textGetoff.visibility = View.VISIBLE
+                btnRide.visibility = View.GONE
+
+                Thread(Runnable {
+                    val alarmGettingOff = db.alarmGettingOffDao()
+                    var bool = alarmGettingOff.isEmptyAlarmGettingOff()
+                    var text = "설정된 알람이 없습니다."
+
+                    if(!bool){
+                        text = alarmGettingOff.getAlarmGetiingOff().route_name
+                    }
+                    runOnUiThread {
+                        if(bool){
+                            textGetoff.text = text
+                            btnGetoff.visibility = View.GONE
+                        }
+                        else{
+                            textGetoff.text = text
+                            btnGetoff.visibility = View.VISIBLE
+                        }
+
+                    }
+                }).start()
+            }
+
+            btnRide.setOnClickListener {
+                Thread(Runnable {
+                    var alarmRidingEntity = db.alarmRidingDao().getAlarmRiding()
+
+                    db.alarmRidingDao().deleteAlarmRiding(alarmRidingEntity)
+
+                    runOnUiThread {
+                        textRide.text = "설정된 알람이 없습니다."
+                        btnRide.visibility = View.GONE
+                    }
+                }).start()
+            }
+
+            btnGetoff.setOnClickListener {
+                Thread(Runnable {
+                    var alarmGettingOffEntity = db.alarmGettingOffDao().getAlarmGetiingOff()
+
+                    db.alarmGettingOffDao().deleteAlarmGettingOff(alarmGettingOffEntity)
+
+                runOnUiThread {
+                    textGetoff.text = "설정된 알람이 없습니다."
+                    btnGetoff.visibility = View.GONE
+                    }
+                }).start()
             }
 
             searchBarMain.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
